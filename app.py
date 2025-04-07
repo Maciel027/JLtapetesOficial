4a7aed4080da606a307e9e9ddb20ef2427a89468
Maciel Sessa <devsessa@gmail.com>
2025-04-07 16:05:37 -0300

    att painel clickes

--- app.py
+++ app.py
@@ -153,28 +153,6 @@ def painel():
         return redirect(url_for('login'))
     return render_template('painel.html')
 
-# ... (demais rotas acima)
-
-# Função para apagar a tabela 'acessos' automaticamente ao iniciar
-def apagar_tabela_acessos_uma_vez():
-    try:
-        con = conectar()
-        cur = con.cursor()
-        cur.execute("DROP TABLE IF EXISTS acessos")
-        con.commit()
-        print("✅ Tabela 'acessos' foi apagada automaticamente no startup.")
-    except Exception as e:
-        print(f"❌ Erro ao apagar a tabela acessos: {e}")
-    finally:
-        con.close()
-
 if __name__ == '__main__':
-    apagar_tabela_acessos_uma_vez()  # <- executa ao subir o app (só 1 vez)
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
-
-
-
-#if __name__ == '__main__':
-#    port = int(os.environ.get('PORT', 8080))
-#    app.run(host='0.0.0.0', port=port)
